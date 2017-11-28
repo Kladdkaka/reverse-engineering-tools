6976f0b8cde37e92caad06c73e9c81feb26e3bcb
Kladdkaka <Kladdkaka@users.noreply.github.com>
2017-11-28 22:02:50 +0100

    added headers to htmldiff because weird sites (outlook lul)

--- html-diff/htmldiff.py
+++ html-diff/htmldiff.py
@@ -22,7 +22,7 @@ authToken = None
 
 
 def get_html():
-    r = requests.get(url)
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'})
     html = HTMLBeautifier.beautify(r.text, 2)
     return html
 
